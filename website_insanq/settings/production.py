aaf45c929dda2a34b67f03f4d37eb97c23b9e573
Alfian Fuadi Rafli <alfian.fuadi@ui.ac.id>
2020-07-20 11:27:53 +0700

    Initial setup - change properties to make deploy to heroku success

--- website_insanq/settings/production.py
+++ website_insanq/settings/production.py
@@ -1,13 +1,17 @@
 import os
 from .base import *
 
-DEBUG = False
+# -- HEROKU NEEDS DEBUG = TRUE --
+# DEBUG = False
+
+# # SECURITY WARNING: define the correct hosts in production!
+# ALLOWED_HOSTS = os.environ["ALLOWED_HOSTS"].split(";")
+
+DEBUG = True
+ALLOWED_HOSTS = []
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ["SECRET_KEY"]
-
-# SECURITY WARNING: define the correct hosts in production!
-ALLOWED_HOSTS = os.environ["ALLOWED_HOSTS"].split(";")
 
 try:
     from .local import *
