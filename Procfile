5f1f8b7d94cb0d504396db8ddecf2b8614c1fe93
Alfian Fuadi Rafli <alfian.fuadi@ui.ac.id>
2020-07-20 14:28:37 +0700

    Initial-setup-2 Solve error on postgres heroku

--- Procfile
+++ Procfile
@@ -1,2 +1,2 @@
-release: python manage.py migrate --noinput
-web: gunicorn website_insanq.wsgi:application
+release: python manage.py migrate
+web: gunicorn website_insanq.wsgi --log-file -
--- website_insanq/settings/production.py
+++ website_insanq/settings/production.py
@@ -15,7 +15,8 @@ ALLOWED_HOSTS = ["*"]
 PRODUCTION = os.environ.get("DATABASE_URL")
 if PRODUCTION:
     DATABASES = {}
-    DATABASES["default"] = dj_database_url.config(conn_max_age=600)
+    DATABASES["default"] = dj_database_url.config()
+SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ["SECRET_KEY"]
