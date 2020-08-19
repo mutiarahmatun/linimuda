7c4843da96d924486baaf0d9ba23fcf3354c1faf
Alfian Fuadi Rafli <alfian.fuadi@ui.ac.id>
2020-08-19 15:51:14 +0700

    Add South to requirements.txt

--- website_insanq_project/settings/prod.py
+++ website_insanq_project/settings/prod.py
@@ -2,7 +2,8 @@ from .base import *  # noqa
 import dj_database_url
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+# TODO
+DEBUG = True
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ["SECRET_KEY"]
