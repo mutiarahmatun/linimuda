44b38df5bc7ba002eba5e6d8d117363c060bfbfa
Alifian Mahardhika <fiansmamda@gmail.com>
2021-04-13 21:47:08 +0900

    fix before deploy using postgre

--- webapps/webapps/wsgi.py
+++ webapps/webapps/wsgi.py
@@ -7,16 +7,10 @@ For more information on this file, see
 https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
 """
 
-# import os
-
-# from django.core.wsgi import get_wsgi_application
-
-# os.environ.setdefault("DJANGO_SETTINGS_MODULE", "webapps.settings.prod")
-
-# application = get_wsgi_application()
-
 import os
+
 from django.core.wsgi import get_wsgi_application
-from dj_static import Cling
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'webapps.settings.prod')
-application = Cling(get_wsgi_application())
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "webapps.settings.dev")
+
+application = get_wsgi_application()
